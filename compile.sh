b904ad8afc3520fc4e34ec87b86a56f6b9b95b13
Aks <lilaks@iMac.attlocal.net>
2025-07-23 22:59:33 -0500

    fix compile.sh to accomodate fresh installs

--- compile.sh
+++ compile.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 build_path=$(readlink -f ./build)
 C_path=$(readlink -f ./src/C)
+
+#check if brew is installed
+if ! command -v brew >/dev/null 2>&1; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv 2>/dev/null || /usr/local/bin/brew shellenv 2>/dev/null)"
+else
+  echo "Homebrew already installed."
+fi
+
+
+# check if gcc is installed
+if ! command -v gcc-15 &> /dev/null; then
+    brew install gcc
+fi
+
 gcc-15 -shared -fPIC -fopenmp -Wall -o $build_path/libmandelbrot.so $C_path/lodepng.c $C_path/color.c $C_path/meta.c $C_path/fractalm.c
 gcc-15  -fopenmp -Wall -o $build_path/mandelbrot $C_path/main.c $C_path/lodepng.c $C_path/color.c $C_path/meta.c $C_path/fractalm.c
