760bc3e1d554b30576d2f4650596112b94f9682d
Aks <lilaks@iMacAks.local>
2025-11-01 20:16:53 -0500

    add check for metal in compile command

--- compile-metal.sh
+++ compile-metal.sh
@@ -1,3 +1,2 @@
-echo "Make sure you have the XCode app from the app store installed!"
 xcrun -sdk macosx metal -c src/metal/Shaders.metal -o build/Shaders.air
 xcrun -sdk macosx metal build/Shaders.air -o build/Shaders.metallib
--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [ ! -f build/libmandelbrot.so ]; then
     ./compile.sh
+    ./compile-metal.sh
 fi
 ./run.sh
