927ca835afbafa89e0105b0e804c294808ce98c5
mario1234 <mario1234@mario1234s-MacBook-Air.local>
2025-11-01 20:14:19 -0500

    add metal setup file

--- compile-metal.sh
+++ compile-metal.sh
@@ -1,3 +1,3 @@
-echo "Make sure you have the XCode app from the app store installed!"
+echo "This program will fail to work if the xcode app is not installed"
 xcrun -sdk macosx metal -c src/metal/Shaders.metal -o build/Shaders.air
 xcrun -sdk macosx metal build/Shaders.air -o build/Shaders.metallib
